c5895b14b572241e7dec7d2d2a40cdbf0b2f9992
jaromtia <tiajarom@gmail.com>
2024-09-05 08:14:48 -0600

    imported packages for home page

---
+++ banking-app/components/DoughtnutChart.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Doughnut } from 'react-chartjs-2';
+
+
+
+const DoughtnutChart = () => {
+  return (
+    <div>
+        <Doughnut data={[]} />
+    </div>
+  )
+}
+
+export default DoughtnutChart
