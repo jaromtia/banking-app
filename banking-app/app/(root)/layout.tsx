3ea872cfae400f2e398100577803a31e592a6d95
tiajarom <tiajarom@gmail.com>
2024-09-06 23:59:53 +0000

    creating mobilenav

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/Sidbar";
-import Image from "next/image"
 import MobileNav from "@/components/MobileNav"
+import Image from "next/image"
+
 
 export default function RootLayout({
   children,
@@ -10,24 +11,18 @@ export default function RootLayout({
   const loggedIn = { firstName: 'Jarom', lastName: 'Tia'}
 
   return (
-    <main>
-        <Sidebar user = { loggedIn } />
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
 
-        <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image
-              src="/icons/logo.svg"
-              width={30}
-              height={30}
-              alt="menu icon"
-            >
-            </Image>
-          </div>
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
-            <MobileNav user = { loggedIn } />
+            <MobileNav user={loggedIn} />
           </div>
-          {children}
         </div>
+        {children}
+      </div>
     </main>
   );
 }
