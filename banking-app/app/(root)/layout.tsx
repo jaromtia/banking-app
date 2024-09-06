03eddffea6fda91d50b7981bec9973c2c9524fdb
tiajarom <tiajarom@gmail.com>
2024-09-06 23:24:39 +0000

    implemented sidebar

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -1,11 +1,32 @@
+import Sidebar from "@/components/Sidbar";
+import Image from "next/image"
+import MobileNav from "@/components/MobileNav"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Jarom', lastName: 'Tia'}
+
   return (
     <main>
-        SIDEBAR
+        <Sidebar user = { loggedIn } />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image
+              src="/icons/logo.svg"
+              width={30}
+              height={30}
+              alt="menu icon"
+            >
+            </Image>
+          </div>
+          <div>
+            <MobileNav user = { loggedIn } />
+          </div>
+        </div>
         {children}
     </main>
   );
