542679a186337637c817ce11a16fe7bf88c50b99
tiajarom <tiajarom@gmail.com>
2024-09-06 23:26:20 +0000

    initialized mobilenav

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
           <div>
             <MobileNav user = { loggedIn } />
           </div>
+          {children}
         </div>
-        {children}
     </main>
   );
 }
--- banking-app/components/MobileNav.tsx
+++ banking-app/components/MobileNav.tsx
@@ -1,2 +1,9 @@
+import React from 'react'
 
+const MobileNav = ({ user }: MobileNavProps) => {
+    return (
+        <div>Mobile Nav</div>
+    )
+}
 
+export default MobileNav
